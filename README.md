84a8f045611d18fb24de1da7c3d80b92e3476c28
kabilovtoha <kabilov2011@gmail.com>
2020-04-15 22:15:29 +0400

    readme pages/paginations

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # Как развернуть проект
 ## Для разработки
+## For development
 ```sh
 docker-compose up -d --build
 ```
-dev superuser:
-##### login: toha
-##### passwd: bqyuio
+##### Dev superuser:
+- auto cteates from fixtures
+###### login: toha
+###### passwd: bqyuio
+
+------------------------
+### Примеры вызовов API
+### API examples
+#### Только для чтения / Only for  read only 
+######  /api/v1/ 
+- Корневой зарос к API, возвращает список дочерних запросов
+
+###### /api/v1/pages/
+- список страниц, каждый елемент которого содержит url на API c детальной информацией
+- list of pages, each element contains API - url to detail info
+
+###### /api/v1/contents/
+- список контента всех страниц, каждый елемент которого содержит url на API c детальной информацией
+- list of contents, each element contains API - url to detail info
--- content_pages/pages/rest_views.py
+++ content_pages/pages/rest_views.py
@@ -2,12 +2,13 @@ from rest_framework import viewsets
 
 from . import models
 from . import serializers
-
+from . import paginations
 
 
 class PageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
+    pagination_class = paginations.CustomPageNumberPagination
 
     def get_serializer_class(self, *args, **kwargs):
         serializer_class = super().get_serializer_class()
@@ -20,6 +21,7 @@ class PageViewSet(viewsets.ReadOnlyModelViewSet):
 class ContentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Content.objects.all()
     serializer_class = serializers.ContentSerializer
+    pagination_class = paginations.CustomPageNumberPagination
 
     def get_serializer_class(self, *args, **kwargs):
         serializer_class = super().get_serializer_class()
